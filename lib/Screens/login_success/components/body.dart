132c974397a0f28ab3dca2f6bbdd298a1085663f
rimzouari97 <salamlem_97>
2021-08-11 17:08:27 +0100

    17:08

--- lib/Screens/login_success/components/body.dart
+++ lib/Screens/login_success/components/body.dart
@@ -9,21 +9,13 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        SizedBox(height: SizeConfig.screenHeight * 0.05),
+        //SizedBox(height: SizeConfig.screenHeight * 0.05),
         Image.asset(
           "assets/images/done.jpg",
           height: SizeConfig.screenHeight * 0.5, //40%
         ),
         SizedBox(height: SizeConfig.screenHeight * 0.08),
-        Text(
-          "Login Success",
-          style: TextStyle(
-            fontSize: getProportionateScreenWidth(30),
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        Spacer(),
+
         SizedBox(
           width: SizeConfig.screenWidth * 0.6,
           child: DefaultButton(
